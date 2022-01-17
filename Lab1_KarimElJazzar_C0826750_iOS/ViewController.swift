9f1aca8df520ed0649e6232ed38b11837cc0eeb8
Karim <c0826750@mylambton.ca>
2022-01-18 00:44:52 +0200

    Initial Commit

---
+++ Lab1_KarimElJazzar_C0826750_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1_KarimElJazzar_C0826750_iOS
+//
+//  Created by Karim El Jazzar on 18/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
